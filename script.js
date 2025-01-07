8f2dbc644493c036d8a724dd294d4cdf8186197a
Grace Judic <gracejudic@MacBook-de-Grace.local>
2025-01-07 17:25:05 +0100

    ajout manifest + modif header

--- script.js
+++ script.js
@@ -1,14 +1,2 @@
 const HEADER_ELMT = document.getElementById("header")
-
 const NIGHT_BUTTON_EXT_ELMT = document.getElementById("night-button")
-NIGHT_BUTTON_EXT_ELMT.textContent = active 
-NIGHT_BUTTON_EXT_ELMT.addEventListener ("click",() => {
-    if(NIGHT_BUTTON_EXT_ELMT.textContent === active) {
-
-    }
-})
-
-function nightModeExt() {
-    
-    
-}
