2864d628e4470bea94c01b961e133c60f8432c2b
Grace Judic <gracejudic@MacBook-de-Grace.local>
2025-01-08 11:11:36 +0100

    début nightmode

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 const HEADER_ELMT = document.getElementById("header")
 const NIGHT_BUTTON_EXT_ELMT = document.getElementById("night-button")
+
+// const buttonTest = document.getElementById("test")
+
+// NIGHT_BUTTON_EXT_ELMT.addEventListener("click",() => {
+//     HEADER_ELMT.classList.add("dark-mode")
+// })
+// bouttonModeJour.addEventListener("click",() => {
+//     HEADER_ELMT.classList.remove("dark-mode")
+// })
