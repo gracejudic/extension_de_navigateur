81b000869503a40e799b8d098d51f90ebc1d1d36
charlene-44 <charlene.3572@gmail.com>
2025-01-09 11:51:58 +0100

    ajout de la fonctionnalité feature change font color qui fonctionne

---
+++ change-font-color.js
@@ -0,0 +1,36 @@
+const BUTTON_FONT_COLOR_CHANGE_ELMT = document.getElementById("font-color-change")
+
+// Variable pour suivre l'état (actif ou inactif)
+let isColorChanged = false;
+
+function changeTextColor(isColorChanged) {
+    const elementsToChange = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'a', 'li', 'ul', 'td', 'span', 'div', 'html', 'body', 'header', 'th', 'strong', 'em', 'main'];
+    elementsToChange.forEach(selector => {
+        document.querySelectorAll(selector).forEach(element => {
+            if (!isColorChanged) {
+                // Appliquer les styles
+                element.style.setProperty('color', '#B4ECC1', 'important');
+                element.style.setProperty('background-color', '#194357', 'important');
+            } else {
+                // Réinitialiser les styles
+                element.style.removeProperty('color');
+                element.style.removeProperty('background-color');
+            }
+        });
+    });
+}
+
+document.addEventListener('DOMContentLoaded',()=>{
+    BUTTON_FONT_COLOR_CHANGE_ELMT.addEventListener('click',() => {
+          // Basculer l'état
+          isColorChanged = !isColorChanged;
+        chrome.tabs.query({active: true, currentWindow: true }, (tabs)=>{
+            chrome.scripting.executeScript({
+                target: {tabId: tabs[0].id},
+                func: changeTextColor,
+                args: [isColorChanged]
+            })
+        })
+    })
+})
+
