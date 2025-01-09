ade2711e251ca46ffa273c7f5c96058addc4832a
abdallahmassangaritoilibou <abdallah.toilbou@laposte.net>
2025-01-09 15:37:18 +0100

    recupere les changement

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const EXTENSION_NAME_ELMT = document.getElementById("extension-name")
 const POWER_IMG_ELMT = document.getElementById("power")
 const MAIN_ELMT = document.getElementById("main")
 const button_test= document.getElementById("test")
-
+const FONT_SIZE_CHANGE_ELMT = document.getElementById("font-size-change")
 function dayMode() {
     HEADER_ELMT.classList.remove("night-mode")
     SWITCH_MODE_IMG_ELMT.classList.remove("night-mode")
@@ -23,13 +23,57 @@ function nightMode (){
 }
 
 let  night_Mode_state = false
-
 NIGHT_BUTTON_EXT_ELMT.addEventListener("click", () =>{
     if(night_Mode_state==true){
         dayMode()
-        night_Mode_state =false;   
+        night_Mode_state =false;      
     } else {
         nightMode()
-        night_Mode_state =true; 
+        night_Mode_state =true;
+          
     }
 })
+
+document.addEventListener('DOMContentLoaded',()=>{
+FONT_SIZE_CHANGE_ELMT.addEventListener('click', function() {
+    const elements = document.getElementsByTagName('*');
+    for (let element of elements) {
+        const currentFontSize = window.getComputedStyle(element).fontSize;
+        if (currentFontSize) {
+            const size = parseFloat(currentFontSize);
+            element.style.fontSize = isLarge ? 
+                `${size * 1.2}px` : //augmente de 20%
+                ''; // retour à la taille par défaut
+        }
+    }
+    isLarge = !isLarge; // Inverser l'état
+});
+})
+
+let 
+
+function Augmenter_Taille_Police() {
+    const CHAGEMENT_TAILLE = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'a',
+         'li', 'ul', 'td', 'span', 'div', 'html', 'body', 'header', 'th', 'strong', 'em', 'main'];
+         CHAGEMENT_TAILLE.forEach(selector).forEach(elements) => {
+        h1.style.color = 'green';
+    });
+}
+
+
+
+// function toggleFontSize() {
+//     const elements = document.getElementsByTagName('*');
+//     for (let element of elements) {
+//         const currentFontSize = window.getComputedStyle(element).fontSize;
+//         if (currentFontSize) {
+//             const size = parseFloat(currentFontSize);
+//             element.style.fontSize = isLarge ? 
+//                 `${size * 1.2}px` : // augmente de 20%
+//                 ''; // retour à la taille par défaut
+//         }
+//     }
+//     isLarge = !isLarge; // Inverser l'état
+// }
+
+   
