43cec45e4e245814d92d833b54aedbe6aa532d98
Dorothee <braud.dorothee@gmail.com>
2025-01-13 11:05:18 +0100

    ajout du tableau des élélments à lire

--- text-to-speech.js
+++ text-to-speech.js
@@ -13,10 +13,10 @@ TEXT_TO_SPEECH_BUTTON_ELMT.addEventListener("click", () => {
 });
 
 function readPageText() {
-    const pageText = document.body.innerText;  // Récupère le texte de la page entière
-    if (pageText.length > 0) {
-        const utterance = new SpeechSynthesisUtterance(pageText);
-        speechSynthesis.speak(utterance);  // Lance la lecture du texte
+    const elementsToRead = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'a', 'li', 'ul', 'td', 'span', 'div', 'html', 'body', 'header', 'th', 'strong', 'em', 'main']
+    if (elementsToRead.length > 0) {
+        const reading = new SpeechSynthesisUtterance(elementsToRead);
+        speechSynthesis.speak(reading);  // Lance la lecture du texte
     } else {
         alert("Il n'y a pas de texte à lire sur cette page.");
     }
