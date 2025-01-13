70e9ced0476a9008f84a47a67ecdcc417ab29e31
charlene-44 <charlene.3572@gmail.com>
2025-01-13 10:20:23 +0100

    changement des couleurs du bouton font color change en vert clair pour le fond et bleu foncé pour le texte

--- change-font-color.js
+++ change-font-color.js
@@ -9,8 +9,8 @@ function changeTextColor(isColorChanged) {
         document.querySelectorAll(selector).forEach(element => {
             if (!isColorChanged) {
                 // Appliquer les styles
-                element.style.setProperty('color', '#B4ECC1', 'important');
-                element.style.setProperty('background-color', '#194357', 'important');
+                element.style.setProperty('color', '#194357', 'important');
+                element.style.setProperty('background-color', '#B4ECC1', 'important');
             } else {
                 // Réinitialiser les styles
                 element.style.removeProperty('color');
